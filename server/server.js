859fc5d6009bf45ec33d1fb7382f6cbd115e19ca
Yashika <yashika2302006@gmail.com>
2025-07-12 16:39:42 +0530

    done

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000', // adjust if your frontend is hosted elsewhere
+  origin: 'http://localhost:5173', // adjust if your frontend is hosted elsewhere
   credentials: true
 }));
 
